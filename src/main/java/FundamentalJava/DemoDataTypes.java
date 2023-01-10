5208e8c15f203778c754d82a86f89662d647b5cd
Manojkumar886 <maddymano434@gmail.com>
2023-01-10 11:12:05 +0530

    datatypes is completed....

---
+++ src/main/java/FundamentalJava/DemoDataTypes.java
@@ -0,0 +1,20 @@
+package FundamentalJava;
+
+public class DemoDataTypes
+{
+    public static void main(String[] args)
+    {
+        short smallestdigit=1234;
+
+        String MyStudentname="Sugan 01 - "+"Rangaraj 02";
+
+        long MYMobileNo=9789355930l;
+        float heights=169;
+        System.out.println("my heights is "+heights);
+        double mypercentage= 88;
+        System.out.println("my 12 th percentage is "+mypercentage);
+        System.out.println("My mobile no is :"+MYMobileNo);
+        System.out.println("my short datatype value is : "+smallestdigit);
+        System.out.println(MyStudentname +" he is new students list"+"\n Thankyou");
+    }
+}
