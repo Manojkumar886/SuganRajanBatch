181617a4ede0c5461c49f97872b12ed148a84316
Manojkumar886 <maddymano434@gmail.com>
2023-01-24 10:52:23 +0530

    Methods and variables is done

--- src/main/java/FundamentalJava/DemoMethodsandVariableTypes/DemoVariables.java
+++ src/main/java/FundamentalJava/DemoMethodsandVariableTypes/DemoVariables.java
@@ -2,17 +2,30 @@ package FundamentalJava.DemoMethodsandVariableTypes;
 
 public class DemoVariables
 {
+   static String IFSCCODE="IDE0E8123";//Static Variable
+    String BranchName="Elachipalayam";
     public void localvariable()
     {
         String AccountHolderName="Manojkumar";//local variable
         System.out.println("Holder name is"+AccountHolderName);
     }
+    public void GlobalAccess()
+    {
+        System.out.println("My branch name is : "+BranchName);//Global Variable
+    }
 
+    public static void Sample()
+    {
+        System.out.println("Thank You");
+    }
     public static void main(String[] args)
     {
         Long AccountNo=9789586765545l;//Instance Variable
         System.out.println("My account no is "+AccountNo);
         DemoVariables local=new DemoVariables();
         local.localvariable();
+        local.GlobalAccess();
+        Sample();
+        System.out.println("my Bank IFSC code is :"+IFSCCODE);
     }
 }
